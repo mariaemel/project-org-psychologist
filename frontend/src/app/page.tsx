5214125daaf1f07db8313bd7c3ea9873734fb743
Настя Кудря <nastakudra44574@gmail.com>
2025-09-30 01:08:58 +0500

    правки

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,7 @@
-import Hero from '@/components/Hero/Hero'
-import About from '@/components/About/About'
-import OrgPsychology from '@/components/OrgPsychology/OrgPsychology'
-import Services from '@/components/Services/Services'
-import ServicesAndTeam from '@/components/ServicesAndTeam/ServicesAndTeam'
-import Breadcrumbs from '@/components/Header/Breadcrumbs';
+'use client';
+
+import Main from '@/components/Main/main';
 
 export default function HomePage() {
-  return (
-    <main>
-      <Breadcrumbs />
-      <Hero />
-      <About />
-      <OrgPsychology />
-      <Services />
-      <ServicesAndTeam />
-    </main>
-  )
+  return <Main />;
 }
